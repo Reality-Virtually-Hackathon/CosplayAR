13a7e58b72a778f7efb2b565b7841ea4d7ccb7f3
Lilika Markatou <markatou@mit.edu>
2017-10-07 14:28:31 -0400

    Flow

---
+++ Assets/Vuforia/Scripts/open.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class open : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+public void op()
+	{
+		SceneManager.LoadScene ("AR Instructions 1");
+	}
+
+
+}
