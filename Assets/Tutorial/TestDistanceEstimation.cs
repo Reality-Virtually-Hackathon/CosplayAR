a5109b0947d823697d427685f836b40929501de0
Ben Taylor <infinityseries@gmail.com>
2017-10-08 15:51:54 -0400

    As close  to perfect as I could get in the little time left

--- Assets/Tutorial/TestDistanceEstimation.cs
+++ Assets/Tutorial/TestDistanceEstimation.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 using Vuforia;
 
 public class TestDistanceEstimation : MonoBehaviour {
-	public GameObject a;
-	public GameObject b;
+    public GameObject a;
+    public GameObject b;
     public GameObject c;
     public GameObject d;
     public GameObject mid;
 
+
+    void Start() {
+        Invoke("Init", 2f);
+    }
+
 	// Use this for initialization
-	void Start () {
+	void Init () {
+        
+        Screen.orientation = ScreenOrientation.LandscapeRight;
 	}
 
     private static Vector3 CalculateMid (Vector3 a, Vector3 b) {
@@ -23,29 +30,41 @@ public class TestDistanceEstimation : MonoBehaviour {
         /*if (a == null || b == null || c == null || mid == null) {
             return;
         }*/
-/*
-        TrackableBehaviour.Status aStatus = a.GetComponent<TrackableBehaviour>().CurrentStatus;
-        TrackableBehaviour.Status bStatus = b.GetComponent<TrackableBehaviour>().CurrentStatus;
-        TrackableBehaviour.Status cStatus = c.GetComponent<TrackableBehaviour>().CurrentStatus;
-        TrackableBehaviour.Status dStatus = d.GetComponent<TrackableBehaviour>().CurrentStatus;
-        */
-/*
-        Vector3 aPosition = a.transform.position;
-        Vector3 bPosition = b.transform.position;
-        Vector3 cPosition = c.transform.position;
-        Vector3 dPosition = d.transform.position;
+        /*
+                TrackableBehaviour.Status aStatus = a.GetComponent<TrackableBehaviour>().CurrentStatus;
+                TrackableBehaviour.Status bStatus = b.GetComponent<TrackableBehaviour>().CurrentStatus;
+                TrackableBehaviour.Status cStatus = c.GetComponent<TrackableBehaviour>().CurrentStatus;
+                TrackableBehaviour.Status dStatus = d.GetComponent<TrackableBehaviour>().CurrentStatus;
+                */
+        /*
+                Vector3 aPosition = a.transform.position;
+                Vector3 bPosition = b.transform.position;
+                Vector3 cPosition = c.transform.position;
+                Vector3 dPosition = d.transform.position;
 
-        Vector3 topMid = CalculateMid(aPosition, bPosition);
-        Vector3 bottomMid = CalculateMid(cPosition, dPosition);
-        Vector3 leftMid = CalculateMid(aPosition, bPosition);
-*/
-        float totalX = a.transform.position.x + b.transform.position.x;
+                Vector3 topMid = CalculateMid(aPosition, bPosition);
+                Vector3 bottomMid = CalculateMid(cPosition, dPosition);
+                Vector3 leftMid = CalculateMid(aPosition, bPosition);
+        */
+        /*float totalX = a.transform.position.x + b.transform.position.x;
         float totalY = a.transform.position.y + c.transform.position.y;
         float totalZ = a.transform.position.z + b.transform.position.z + c.transform.position.z;
         Vector3 midPosition = new Vector3(
             totalX/2,
             totalY/2,
             totalZ/3
+        );
+        //Vector3 total = a.transform.position + b.transform.position + c.transform.position;// + d.transform.position;
+        //Vector3 midPosition = total/3;
+        mid.transform.position = midPosition;*/
+
+        float totalX = a.transform.position.x + b.transform.position.x;
+        float totalY = a.transform.position.y;// + b.transform.position.y + c.transform.position.z;
+        float totalZ = a.transform.position.z + c.transform.position.z;
+        Vector3 midPosition = new Vector3(
+            totalX / 2,
+            totalY / 1,//3,
+            totalZ / 2
         );
         //Vector3 total = a.transform.position + b.transform.position + c.transform.position;// + d.transform.position;
         //Vector3 midPosition = total/3;
@@ -65,11 +84,11 @@ public class TestDistanceEstimation : MonoBehaviour {
         Vector3 normal = GetNormal(a.transform.position, b.transform.position, c.transform.position);
         Vector3 lookAt = midPosition + normal * 10;
 
-        mid.transform.localEulerAngles = Vector3.zero;
-        mid.transform.LookAt(lookAt);
-        mid.transform.localEulerAngles += new Vector3(0, 0, -90);
+        mid.transform.localEulerAngles = new Vector3(90, 0, 0);
+        //mid.transform.LookAt(lookAt);
+        //mid.transform.localEulerAngles += new Vector3(0, 0, -90);
 
-        float theta = Mathf.Atan2(
+        /*float theta = Mathf.Atan2(
             a.transform.position.y-b.transform.position.y,
             a.transform.position.x-b.transform.position.x
         ) * Mathf.Rad2Deg;
@@ -80,9 +99,28 @@ public class TestDistanceEstimation : MonoBehaviour {
             mid.transform.Rotate(new Vector3(0, 0, -theta-90), Space.Self);
         } else {
             mid.transform.Rotate(new Vector3(0, 0, +theta+90), Space.Self);
+        }*/
+
+        float theta = Mathf.Atan2(
+            
+            a.transform.position.z - b.transform.position.z,
+            a.transform.position.x - b.transform.position.x
+        ) * Mathf.Rad2Deg;
+
+        //mid.transform.Rotate(new Vector3(0, 0, -theta - 90), Space.Self);
+        //mid.transform.RotateAround(transform.position, normal, -theta);
+        if (theta < 90 && theta > -90)
+        {
+            mid.transform.Rotate(new Vector3(0, 0, +theta + 90), Space.Self);
+            //mid.transform.Rotate(new Vector3(-theta - 90, 0, 0), Space.Self);
+        }
+        else
+        {
+            mid.transform.Rotate(new Vector3(0, 0, -theta - 90), Space.Self);
+            //mid.transform.Rotate(new Vector3(+theta + 90, 0, 0), Space.Self);
         }
 
-	}
+    }
     Vector3 GetNormal(Vector3 a, Vector3 b, Vector3 c) {
         Vector3 side1 = b - a;
         Vector3 side2 = c - a;
@@ -160,7 +198,7 @@ public class TestDistanceEstimation : MonoBehaviour {
         V3("Mid Rotation", mid.transform.localEulerAngles);
 
         float theta = Mathf.Atan2(
-            a.transform.position.y-b.transform.position.y,
+            a.transform.position.z-b.transform.position.z,
             a.transform.position.x-b.transform.position.x
         ) * Mathf.Rad2Deg;
         GUILayout.Button("Theta: " + theta.ToString());
