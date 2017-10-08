cbfa7d7e14279d6a23f411ae6b485cad01dd990d
gfaline <gwenf@mit.edu>
2017-10-08 11:53:51 -0400

    Make new project just with the app page stuff called TempCosplay
    It removed the Vuforia errors that came from the old project.

---
+++ TempCosplay/Assets/Scripts/PageChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PageChanger : MonoBehaviour {
+	Sprite[] pages;
+	int place;
+
+	public Image curPage;
+	// Use this for initialization
+	void Start(){
+		place = 0;
+		object[] pageHolder = (Resources.LoadAll ("SizingPages"));
+		pages = new Sprite[pageHolder.Length];
+		for(int i = 0; i<pageHolder.Length; i++) {
+			pages[i] = (Sprite)(pageHolder[i]);
+		}
+	}
+
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown (0)) 
+			NextPage ();
+	}
+
+	public void NextPage(){
+		if (place == pages.Length - 1)
+			place = 0;
+		curPage.sprite = pages [place];
+
+	}
+}
---
+++ TempCosplay/Assets/Scripts/WaistChange.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaistChange : MonoBehaviour {
+
+	public Text material;
+	public Text drawsting;
+
+	public int mod = 1;
+
+	// Use this for initialization
+	void Start(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void materialUp(){
+		int x = int.Parse(material.text);
+		x += mod;
+		material.text = x.ToString();
+	}
+
+	public void materialDown(){
+		int x = int.Parse(material.text);
+		x -= mod;
+		material.text = x.ToString();
+	}
+
+	public void drawstringUp(){
+		int x = int.Parse(drawsting.text);
+		x += mod;
+		drawsting.text = x.ToString();
+	}
+
+	public void drawstringDown(){
+		int x = int.Parse(drawsting.text);
+		x -= mod;
+		drawsting.text = x.ToString();
+	}
+}
