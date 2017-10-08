daf2ece024dc5f2d062ac8df422834f7a450bf5b
Ben Taylor <infinityseries@gmail.com>
2017-10-08 13:55:25 -0400

    Unity 2017.2 port adnd vr support

---
+++ Assets/MixedRealityMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixedRealityMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //Vuforia.VideoBackgroundManager.Instance.SetVideoBackgroundEnabled(true);
+        //Vuforia.MixedRealityController.Instance.SetMode(Vuforia.MixedRealityController.Mode.ROTATIONAL_VIEWER_AR);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
